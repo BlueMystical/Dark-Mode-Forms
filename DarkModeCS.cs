188087790500efe95ad7f72a6327b4077e625c45
BlueMystic <jhollman.chacon@gmail.com>
2024-02-06 17:32:36 -0300

    1.0.2

--- DarkModeCS.cs
+++ DarkModeCS.cs
@@ -501,6 +501,7 @@ namespace DarkModeForms
 			{
 				combo.FlatStyle = FStyle;
 				combo.BackColor = OScolors.Control;
+				control.GetType().GetProperty("ButtonColor")?.SetValue(control, OScolors.Surface);				
 				combo.Invalidate();
 			}
 			if (control is MenuStrip menu)
@@ -682,7 +683,7 @@ namespace DarkModeForms
 			Rectangle bounds = new Rectangle(Point.Empty, e.Item.Size);
 
 			Color gradientBegin = MyColors.Background; // Color.FromArgb(203, 225, 252);
-			Color gradientEnd = MyColors.Background; 
+			Color gradientEnd = MyColors.Background;
 
 			Pen BordersPencil = new Pen(MyColors.Background);
 
@@ -690,12 +691,12 @@ namespace DarkModeForms
 			if (button.Pressed || button.Checked)
 			{
 				gradientBegin = MyColors.Control;
-				gradientEnd = MyColors.Control; 
+				gradientEnd = MyColors.Control;
 			}
 			else if (button.Selected)
 			{
 				gradientBegin = MyColors.Accent;
-				gradientEnd = MyColors.Accent; 
+				gradientEnd = MyColors.Accent;
 			}
 
 			using (Brush b = new LinearGradientBrush(
@@ -708,18 +709,18 @@ namespace DarkModeForms
 			}
 
 			e.Graphics.DrawRectangle(
-				BordersPencil, 
+				BordersPencil,
 				bounds);
 
 			g.DrawLine(
-				BordersPencil, 
+				BordersPencil,
 				bounds.X,
 				bounds.Y,
 				bounds.Width - 1,
 				bounds.Y);
 
 			g.DrawLine(
-				BordersPencil, 
+				BordersPencil,
 				bounds.X,
 				bounds.Y,
 				bounds.X,
@@ -746,7 +747,7 @@ namespace DarkModeForms
 			Graphics g = e.Graphics;
 			Rectangle bounds = new Rectangle(Point.Empty, e.Item.Size);
 			Color gradientBegin = MyColors.Background; // Color.FromArgb(203, 225, 252);
-			Color gradientEnd = MyColors.Background; 
+			Color gradientEnd = MyColors.Background;
 
 			Pen BordersPencil = new Pen(MyColors.Background);
 
@@ -772,7 +773,7 @@ namespace DarkModeForms
 				e.Graphics.FillRectangle(b, bounds);
 			}
 
-			
+
 			//3. Draws the Chevron:
 			#region Chevron
 
@@ -794,18 +795,18 @@ namespace DarkModeForms
 		{
 			Rectangle bounds = new Rectangle(Point.Empty, e.Item.Size);
 			Color gradientBegin = MyColors.Background; // Color.FromArgb(203, 225, 252);
-			Color gradientEnd = MyColors.Background; 
+			Color gradientEnd = MyColors.Background;
 
 			//1. Determine the colors to use:
-			if (e.Item.Pressed) 
+			if (e.Item.Pressed)
 			{
-				gradientBegin = MyColors.Control; 
+				gradientBegin = MyColors.Control;
 				gradientEnd = MyColors.Control;
 			}
 			else if (e.Item.Selected)
 			{
 				gradientBegin = MyColors.Accent;
-				gradientEnd = MyColors.Accent; 
+				gradientEnd = MyColors.Accent;
 			}
 
 			//2. Draw the Box around the Control
@@ -824,9 +825,9 @@ namespace DarkModeForms
 			int Padding = 2; //<- From the right side
 			Size cSize = new Size(8, 4); //<- Size of the Chevron: 8x4 px
 			Pen ChevronPen = new Pen(MyColors.TextInactive, 2); //<- Color and Border Width
-			Point P1 = new Point(bounds.Width - (cSize.Width + Padding),	(bounds.Height / 2) - (cSize.Height / 2));
-			Point P2 = new Point(bounds.Width - Padding,					(bounds.Height / 2) - (cSize.Height / 2));
-			Point P3 = new Point(bounds.Width - (cSize.Width / 2 + Padding),(bounds.Height / 2) + (cSize.Height / 2));
+			Point P1 = new Point(bounds.Width - (cSize.Width + Padding), (bounds.Height / 2) - (cSize.Height / 2));
+			Point P2 = new Point(bounds.Width - Padding, (bounds.Height / 2) - (cSize.Height / 2));
+			Point P3 = new Point(bounds.Width - (cSize.Width / 2 + Padding), (bounds.Height / 2) + (cSize.Height / 2));
 
 			e.Graphics.DrawLine(ChevronPen, P1, P3);
 			e.Graphics.DrawLine(ChevronPen, P2, P3);
@@ -979,196 +980,204 @@ namespace DarkModeForms
 		}
 	}
 
-	public class FlatCombo : ComboBox
-    {
-		private const int WM_PAINT = 0xF;
-		private const int WM_CTLCOLORSTATIC = 0x13;
-
-		private int buttonWidth = SystemInformation.HorizontalScrollBarArrowWidth;
-		private Color borderColor = Color.Blue;
-
+	// https://github.com/r-aghaei/FlatComboExample/tree/master
+	public class FlatComboBox : ComboBox
+	{
+		private Color borderColor = Color.Gray;
+		[DefaultValue(typeof(Color), "Gray")]
 		public Color BorderColor
 		{
 			get { return borderColor; }
-			set { borderColor = value; Invalidate(); }
-		}
-		protected override void WndProc(ref Message m)
-		{
-			base.WndProc(ref m);
-			if (m.Msg == WM_PAINT && DropDownStyle != ComboBoxStyle.Simple)
-			{
-				using (var g = Graphics.FromHwnd(Handle))
-				{
-					var adjustMent = 0;
-					if (FlatStyle == FlatStyle.Popup ||
-					   (FlatStyle == FlatStyle.Flat &&
-					   DropDownStyle == ComboBoxStyle.DropDownList))
-						adjustMent = 1;
-					var innerBorderWisth = 3;
-					var innerBorderColor = BackColor;
-					if (DropDownStyle == ComboBoxStyle.DropDownList &&
-						(FlatStyle == FlatStyle.System || FlatStyle == FlatStyle.Standard))
-						innerBorderColor = Color.FromArgb(0xCCCCCC);
-					if (DropDownStyle == ComboBoxStyle.DropDown && !Enabled)
-						innerBorderColor = SystemColors.Control;
-
-					if (DropDownStyle == ComboBoxStyle.DropDown || Enabled == false)
-					{
-						using (var p = new Pen(innerBorderColor, innerBorderWisth))
-						{
-							p.Alignment = System.Drawing.Drawing2D.PenAlignment.Inset;
-							g.DrawRectangle(p, 1, 1,
-								Width - buttonWidth - adjustMent - 1, Height - 1);
-						}
-					}
-					using (var p = new Pen(BorderColor))
-					{
-						g.DrawRectangle(p, 0, 0, Width - 1, Height - 1);
-						g.DrawLine(p, Width - buttonWidth - adjustMent,
-							0, Width - buttonWidth - adjustMent, Height);
-					}
-					//custom DropDown Button
-					//var X = new Point(Width - buttonWidth - 1);
-					//Rectangle bounds = new Rectangle(X, new Size(30, 30));
-					//using (Brush b = new SolidBrush(Color.Azure))
-					//{
-					//	g.FillRectangle(b, bounds);
-					//}
-				}
-			}
-			if (m.Msg == WM_CTLCOLORSTATIC)
-			{
-				//----------------------------
-				using (var g = Graphics.FromHwnd(Handle))
-				{
-					using (var p = new Pen(Color.FromArgb(93, 100, 103)))
-					{
-						// its a fat border so we need to draw 3 rectangles to cover it
-						g.DrawRectangle(p, 0, 0, Width - 1, Height - 1);
-						g.DrawRectangle(p, 1, 1, Width - 3, Height - 3);
-						g.DrawRectangle(p, 2, 2, Width - 5, Height - 5);
-					}
-				}
-				//----------------------------
-			}
-
-		}
-
-
-	}
-
-	[DesignerCategory("code")]
-	public partial class ComboBoxExt : ComboBox
-	{
-
-		private ListNativeWindow listControl = null;
-		private Color m_ListBorderColor = Color.Transparent;
-
-		public ComboBoxExt() { }
-
-		[DefaultValue(typeof(Color), "Transparent")]
-		public Color ListBorderColor
-		{
-			get
-			{
-				return m_ListBorderColor;
-			}
 			set
 			{
-				m_ListBorderColor = value;
-				if (listControl is not null)
+				if (borderColor != value)
 				{
-					listControl.BorderColor = m_ListBorderColor;
+					borderColor = value;
+					Invalidate();
 				}
 			}
 		}
-
-		protected override void OnHandleCreated(EventArgs e)
+		private Color buttonColor = Color.LightGray;
+		[DefaultValue(typeof(Color), "LightGray")]
+		public Color ButtonColor
 		{
-			base.OnHandleCreated(e);
-			listControl = new ListNativeWindow(GetComboBoxListInternal(Handle));
-			listControl.BorderColor = ListBorderColor;
-		}
-
-		protected override void OnHandleDestroyed(EventArgs e)
-		{
-			listControl.ReleaseHandle();
-			base.OnHandleDestroyed(e);
-		}
-
-		public partial class ListNativeWindow : NativeWindow
-		{
-			public ListNativeWindow() : this(IntPtr.Zero) { }
-
-			public ListNativeWindow(IntPtr hWnd)
+			get { return buttonColor; }
+			set
 			{
-				if (hWnd != IntPtr.Zero) AssignHandle(hWnd);
+				if (buttonColor != value)
+				{
+					buttonColor = value;
+					Invalidate();
+				}
 			}
-
-			public Color BorderColor { get; set; } = Color.Transparent;
-
-			protected override void WndProc(ref Message m)
+		}
+		
+		protected override void WndProc(ref Message m)
+		{
+			if (m.Msg == WM_PAINT && DropDownStyle != ComboBoxStyle.Simple)
 			{
+				var clientRect = ClientRectangle;
+				var dropDownButtonWidth = SystemInformation.HorizontalScrollBarArrowWidth;
+				var outerBorder = new Rectangle(clientRect.Location,
+					new Size(clientRect.Width - 1, clientRect.Height - 1));
+				var innerBorder = new Rectangle(outerBorder.X + 1, outerBorder.Y + 1,
+					outerBorder.Width - dropDownButtonWidth - 2, outerBorder.Height - 2);
+				var innerInnerBorder = new Rectangle(innerBorder.X + 1, innerBorder.Y + 1,
+					innerBorder.Width - 2, innerBorder.Height - 2);
+				var dropDownRect = new Rectangle(innerBorder.Right + 1, innerBorder.Y,
+					dropDownButtonWidth, innerBorder.Height + 1);
+				if (RightToLeft == RightToLeft.Yes)
+				{
+					innerBorder.X = clientRect.Width - innerBorder.Right;
+					innerInnerBorder.X = clientRect.Width - innerInnerBorder.Right;
+					dropDownRect.X = clientRect.Width - dropDownRect.Right;
+					dropDownRect.Width += 1;
+				}
+				var innerBorderColor = Enabled ? BackColor : SystemColors.Control;
+				var outerBorderColor = Enabled ? BorderColor : SystemColors.ControlDark;
+				var buttonColor = Enabled ? ButtonColor : SystemColors.Control;
+				var middle = new Point(dropDownRect.Left + dropDownRect.Width / 2,
+					dropDownRect.Top + dropDownRect.Height / 2);
+				var arrow = new Point[]
+				{
+					new Point(middle.X - 3, middle.Y - 2),
+					new Point(middle.X + 4, middle.Y - 2),
+					new Point(middle.X, middle.Y + 2)
+				};
+				var ps = new PAINTSTRUCT();
+				bool shoulEndPaint = false;
+				IntPtr dc;
+				if (m.WParam == IntPtr.Zero)
+				{
+					dc = BeginPaint(Handle, ref ps);
+					m.WParam = dc;
+					shoulEndPaint = true;
+				}
+				else
+				{
+					dc = m.WParam;
+				}
+				var rgn = CreateRectRgn(innerInnerBorder.Left, innerInnerBorder.Top,
+					innerInnerBorder.Right, innerInnerBorder.Bottom);
+				SelectClipRgn(dc, rgn);
+				DefWndProc(ref m);
+				DeleteObject(rgn);
+				rgn = CreateRectRgn(clientRect.Left, clientRect.Top,
+					clientRect.Right, clientRect.Bottom);
+				SelectClipRgn(dc, rgn);
+				using (var g = Graphics.FromHdc(dc))
+				{
+					g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+					g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+					g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+					#region DropDown Button
+
+					using (var b = new SolidBrush(buttonColor))
+					{
+						g.FillRectangle(b, dropDownRect);
+					}
+
+					#endregion
+
+					#region Chevron
+
+					Size cSize = new Size(8, 4); //<- Size of the Chevron: 8x4 px
+					var chevron = new Point[]
+					{
+						new Point(middle.X - (cSize.Width / 2), middle.Y - (cSize.Height / 2)),
+						new Point(middle.X + (cSize.Width / 2), middle.Y - (cSize.Height / 2)),
+						new Point(middle.X, middle.Y + (cSize.Height / 2))
+					};
+					using (var chevronPen = new Pen(BorderColor, 2.5f)) //<- Color and Border Width
+					{
+						g.DrawLine(chevronPen, chevron[0], chevron[2]);
+						g.DrawLine(chevronPen, chevron[1], chevron[2]);
+					}
+
+					//Replaced 'arrow' triangle with a Chevron
+					//using (var b = new SolidBrush(outerBorderColor))
+					//{
+					//	g.FillPolygon(b, arrow);
+					//}
+
+					#endregion
+
+					#region Borders
+
+					using (var p = new Pen(innerBorderColor))
+					{
+						g.DrawRectangle(p, innerBorder);
+						g.DrawRectangle(p, innerInnerBorder);
+					}
+					using (var p = new Pen(outerBorderColor))
+					{
+						g.DrawRectangle(p, outerBorder);
+					}
+
+					#endregion
+
+				}
+				if (shoulEndPaint)
+					EndPaint(Handle, ref ps);
+				DeleteObject(rgn);
+			}
+			else
 				base.WndProc(ref m);
-				switch (m.Msg)
-				{
-					case WM_NCPAINT:
-						{
-							var hDC = GetWindowDC(Handle);
-							try
-							{
-								using (var g = Graphics.FromHdc(hDC))
-								using (var pen = new Pen(BorderColor))
-								{
-									var rect = g.VisibleClipBounds;
-									g.DrawRectangle(pen, 0f, 0f, rect.Width - 1f, rect.Height - 1f);
-								}
-							}
-							finally
-							{
-								ReleaseDC(Handle, hDC);
-							}
-							m.Result = IntPtr.Zero;
-							break;
-						}
-				}
-			}
 		}
 
-
-		[DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
-		internal static extern bool GetComboBoxInfo(IntPtr hWnd, ref COMBOBOXINFO pcbi);
-
-		[DllImport("user32.dll", SetLastError = true)]
-		internal static extern IntPtr GetWindowDC(IntPtr hWnd);
-
-		[DllImport("user32.dll", SetLastError = true)]
-		internal static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDc);
-
-		internal const int WM_NCPAINT = 0x85;
-
+		private const int WM_PAINT = 0xF;
 		[StructLayout(LayoutKind.Sequential)]
-		internal partial struct COMBOBOXINFO
+		public struct RECT
 		{
-			public int cbSize;
-			public Rectangle rcItem;
-			public Rectangle rcButton;
-			public int buttonState;
-			public IntPtr hwndCombo;
-			public IntPtr hwndEdit;
-			public IntPtr hwndList;
-			public void Init()
-			{
-				cbSize = Marshal.SizeOf<COMBOBOXINFO>();
-			}
+			public int L, T, R, B;
 		}
+		[StructLayout(LayoutKind.Sequential)]
+		public struct PAINTSTRUCT
+		{
+			public IntPtr hdc;
+			public bool fErase;
+			public int rcPaint_left;
+			public int rcPaint_top;
+			public int rcPaint_right;
+			public int rcPaint_bottom;
+			public bool fRestore;
+			public bool fIncUpdate;
+			public int reserved1;
+			public int reserved2;
+			public int reserved3;
+			public int reserved4;
+			public int reserved5;
+			public int reserved6;
+			public int reserved7;
+			public int reserved8;
+		}
+		[DllImport("user32.dll")]
+		private static extern IntPtr BeginPaint(IntPtr hWnd,
+			[In, Out] ref PAINTSTRUCT lpPaint);
 
-		internal IntPtr GetComboBoxListInternal(IntPtr cboHandle)
+		[DllImport("user32.dll")]
+		private static extern bool EndPaint(IntPtr hWnd, ref PAINTSTRUCT lpPaint);
+
+		[DllImport("gdi32.dll")]
+		public static extern int SelectClipRgn(IntPtr hDC, IntPtr hRgn);
+
+		[DllImport("user32.dll")]
+		public static extern int GetUpdateRgn(IntPtr hwnd, IntPtr hrgn, bool fErase);
+		public enum RegionFlags
 		{
-			var cbInfo = new COMBOBOXINFO();
-			cbInfo.Init();
-			GetComboBoxInfo(cboHandle, ref cbInfo);
-			return cbInfo.hwndList;
+			ERROR = 0,
+			NULLREGION = 1,
+			SIMPLEREGION = 2,
+			COMPLEXREGION = 3,
 		}
+		[DllImport("gdi32.dll")]
+		internal static extern bool DeleteObject(IntPtr hObject);
+
+		[DllImport("gdi32.dll")]
+		private static extern IntPtr CreateRectRgn(int x1, int y1, int x2, int y2);
 	}
+
+
+	
 }
