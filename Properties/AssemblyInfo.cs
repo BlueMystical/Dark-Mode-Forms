ced685e52c59d3fcf1187be2ae61fe6702ea8ac0
Frost <frostchi6688@gmail.com>
2024-07-18 12:19:10 -0500

    SDK Style Proj, Setup for Nuget

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,17 +1,5 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// La información general de un ensamblado se controla mediante el siguiente
-// conjunto de atributos. Cambie estos valores de atributo para modificar la información
-// asociada con un ensamblado.
-[assembly: AssemblyTitle("DarkModeForms")]
-[assembly: AssemblyDescription("Example")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BlueMystic")]
-[assembly: AssemblyProduct("DarkModeForms")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles
 // para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde
@@ -20,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
 [assembly: Guid("3972405e-252e-4eae-97b7-204957260480")]
-
-// La información de versión de un ensamblado consta de los cuatro valores siguientes:
-//
-//      Versión principal
-//      Versión secundaria
-//      Número de compilación
-//      Revisión
-//
-// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
-// utilizando el carácter "*", como se muestra a continuación:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
