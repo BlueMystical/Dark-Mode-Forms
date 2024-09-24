c023968c9fadbcb6db9eec56c33a087e17dc9b9c
Andrei Krasutski <info@krasutski.com>
2024-09-24 21:44:53 +0300

    Configuring your Windows Forms app for high DPI support

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -893,8 +893,8 @@ namespace DarkModeForms
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
 			this.BackColor = System.Drawing.SystemColors.Control;
 			this.ClientSize = new System.Drawing.Size(730, 671);
 			this.Controls.Add(this.textBox4);
