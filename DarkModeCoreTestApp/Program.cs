aab62f8007d6eb640d2dc51ccaf424a4eec4e2a2
OkieDan <danp129@gmail.com>
2025-03-22 19:29:17 -0500

    Before converting DarkModeCoreTestApp

---
+++ DarkModeCoreTestApp/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace DarkModeCoreTestApp
+{
+	internal static class Program
+	{
+		/// <summary>
+		/// Punto de entrada principal para la aplicación.
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+		}
+	}
+}
---
+++ DarkModeCoreTestApp/Screenshots/README.md
@@ -0,0 +1,1 @@
+Just various ScreenShots.
